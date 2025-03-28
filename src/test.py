afa263f68dcf41bd9333eb83170441341f723ba2
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2025-03-28 16:47:52 +0100

    Plot episode reward

--- src/test.py
+++ src/test.py
@@ -2,6 +2,8 @@ from algorithm.tdmpc import TDMPC
 from pathlib import Path
 from cfg import parse_cfg
 from env import make_env
+import matplotlib.pyplot as plt
+
 __CONFIG__, __LOGS__ = 'cfgs', 'logs'
 
 def test(cfg):
@@ -10,13 +12,21 @@ def test(cfg):
     agent.load(model_dir / 'model.pt')
     obs, _ = env.reset()
     done, ep_reward, t = False, 0, 0
+    rewards = []
     while not done:
         action = agent.plan(obs, eval_mode=True, step=50000, t0=t==0)
         obs, reward, terminated, truncated, _ = env.step(action.cpu().numpy())
         done = terminated or truncated
         ep_reward += reward
+        rewards.append(reward)
         t += 1
     print(ep_reward)
+
+    # Plot the rewards
+    plt.plot(rewards)
+    plt.xlabel('Step')
+    plt.ylabel('Reward')
+    plt.show()
 
 if __name__ == '__main__':
     test(parse_cfg(Path().cwd() / __CONFIG__))
