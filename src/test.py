07c1709c416144af31cfbdd5b5539745ee8bba23
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2025-04-16 19:10:45 +0200

    Make test for any environment

--- src/test.py
+++ src/test.py
@@ -7,9 +7,68 @@ import numpy as np
 
 __CONFIG__, __LOGS__ = 'cfgs', 'logs'
 
-class PlotTest():
-    def __init__(self):
-        return
+class Test():
+    def __init__(self, cfg):
+        self.env = make_env(cfg, render_mode=None)
+        self.agent = TDMPC(cfg)
+        self.num_episodes = 10
+
+        # Load the model
+        model_dir = Path().cwd() / __LOGS__ / cfg.task / 'models'
+        self.agent.load(model_dir / 'model.pt')
+
+        self.step = cfg.train_steps
+
+    def run(self):
+        episode_rewards = []
+        for episode in range(self.num_episodes):
+            obs, _ = self.env.reset()
+            done, ep_reward, t = False, 0, 0
+            while not done:
+                action = self.agent.plan(obs, eval_mode=True, step=self.step, t0=t==0)
+                obs, reward, terminated, truncated, _ = self.env.step(action.cpu().numpy())
+                done = terminated or truncated
+                ep_reward += reward
+                t += 1
+        episode_rewards.append(ep_reward)
+
+        print(f"Average Episode Reward: {np.nanmean(episode_rewards):.2f}")
+
+class TestPMSM(Test):
+    def __init__(self, cfg):
+        super().__init__(cfg)
+
+    # Create plots directory if it does not exist
+    plots_dir = Path().cwd() / 'plots'
+    plots_dir.mkdir(exist_ok=True)
+
+    def run(self):
+        episode_rewards = []
+        for episode in range(self.num_episodes):
+            obs, _ = self.env.reset(options={"Idref":0, "Iqref":100}) 
+            action_list = []
+            reward_list = []
+            state_list  = [obs[0:4]]
+
+            plt.figure(episode, figsize=(10, 6))
+            done, ep_reward, t = False, 0, 0
+            while not done:
+                action = self.agent.plan(obs, eval_mode=True, step=self.step, t0=t==0)
+                obs, reward, terminated, truncated, _ = self.env.step(action.cpu().numpy())
+                done = terminated or truncated
+
+                if not done:
+                    action_list.append(action.cpu().numpy())
+                    state_list.append(obs[0:4])
+                    reward_list.append(reward)
+                ep_reward += reward
+                t += 1
+
+            episode_rewards.append(ep_reward)
+            self.plot_three_phase(episode, state_list, action_list, reward_list,
+                                    "TDMPC", "absolute", 200*2*np.pi*obs[4])
+
+        print(f"Average Episode Reward: {np.nanmean(episode_rewards):.2f}")
 
     def plot_three_phase(self, idx, observations, actions, reward, env_name, reward_type, speed=None):
         plt.clf()
@@ -42,47 +101,11 @@ class PlotTest():
                          box.width, box.height * 0.9])
 
         plt.savefig(f"plots/{env_name}_{idx}.png", bbox_inches='tight')
-        plt.pause(0.001)  # pause a bit so that plots are updated
-
-        # plt.show()
-
-def test(cfg):
-    env, agent = make_env(cfg, render_mode=None), TDMPC(cfg)
-    model_dir = Path().cwd() / __LOGS__ / cfg.task / 'models'
-    agent.load(model_dir / 'model.pt')
-
-    # Create plots directory if it does not exist
-    plots_dir = Path().cwd() / 'plots'
-    plots_dir.mkdir(exist_ok=True)
-    plot = PlotTest()
-
-    num_episodes = 10
-    episode_rewards = []
-    for episode in range(num_episodes):
-        obs, _ = env.reset(options={"Idref":0, "Iqref":100}) 
-        action_list = []
-        reward_list = []
-        state_list  = [obs[0:4]]
-
-        plt.figure(episode, figsize=(10, 6))
-        done, ep_reward, t = False, 0, 0
-        while not done:
-            action = agent.plan(obs, eval_mode=True, step=2501, t0=t==0)
-            obs, reward, terminated, truncated, _ = env.step(action.cpu().numpy())
-            done = terminated or truncated
-
-            if not done:
-                action_list.append(action.cpu().numpy())
-                state_list.append(obs[0:4])
-                reward_list.append(reward)
-            ep_reward += reward
-            t += 1
-
-        episode_rewards.append(ep_reward)
-        plot.plot_three_phase(episode, state_list, action_list, reward_list,
-                                "TDMPC", "absolute", 200*2*np.pi*obs[4])
-        plt.close()
-    print(f"Average Episode Reward: {np.nanmean(episode_rewards):.2f}")
 
 if __name__ == '__main__':
-    test(parse_cfg(Path().cwd() / __CONFIG__))
+    cfg = parse_cfg(Path().cwd() / __CONFIG__)
+    if cfg.task == 'PMSM-v0':
+        test = TestPMSM(cfg)
+    else:
+        test = Test(cfg)
+    test.run()
