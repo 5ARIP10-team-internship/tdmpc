b02efdd2d73e3ae80ed1159983e4689dacc2f74b
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2025-05-14 13:58:35 +0200

    Add job array file for hpc

--- array.sh
+++ array.sh
@@ -2,12 +2,13 @@
 
 #SBATCH --partition=gpu_mig
 #SBATCH --gpus=1
-#SBATCH --job-name=TrainTDMPC
+#SBATCH --job-name=ArrayTDMPC
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=02:00:00
-#SBATCH --output=slurm_output_%A.txt
-#SBATCH --error=slurm_error_%A.txt
+#SBATCH --array=1-2%2
+#SBATCH --time=04:00:00
+#SBATCH --output=slurm_output_%A_%a.txt
+#SBATCH --error=slurm_error_%A_%a.txt
 
 # Load necessary modules (adjust based on your environment)
 module purge
@@ -18,16 +19,19 @@ module load matplotlib/3.7.2-gfbf-2023a
 source tdmpc-env/bin/activate  # activate your virtual environment
 
 # Define the directory where to save the models, and copy the job file to it
-JOB_FILE=$HOME/tdmpc/train_hpc.sh
+JOB_FILE=$HOME/tdmpc/array.sh
+HPARAMS_FILE=$HOME/tdmpc/array_hyperparameters.txt
 TASK_NAME=$(echo $1 | cut -d'-' -f1)
 CFG_FILE=$HOME/tdmpc/cfgs/tasks/${TASK_NAME}.yaml
 
-LOGDIR=logs/train_${SLURM_JOB_ID}
+LOGDIR=logs/array_${SLURM_ARRAY_JOB_ID}
 LOGPATH=$HOME/tdmpc/${LOGDIR}
 
 mkdir -p $LOGPATH
 rsync $CFG_FILE $LOGPATH/
 rsync $JOB_FILE $LOGPATH/
+rsync $HPARAMS_FILE $LOGPATH/
 
 # Run the code
-srun python src/train.py task=$1 checkpoint_dir=$LOGDIR/${TASK_NAME}
+srun python src/train.py task=$1 checkpoint_dir=$LOGDIR/experiment_${SLURM_ARRAY_TASK_ID} \
+            $(head -$SLURM_ARRAY_TASK_ID $HPARAMS_FILE | tail -1)
