5d6d8d00f91edb804c25ae357f40e990ae4a599e
Tom van Eemeren <t.v.eemeren@student.tue.nl>
2025-05-31 17:56:04 +0200

    Make test script more general

--- test_hpc.sh
+++ test_hpc.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=TestTDMPC
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=00:05:00
+#SBATCH --time=02:00:00
 #SBATCH --output=logs/slurm_output_%A.txt
 #SBATCH --error=logs/slurm_error_%A.txt
 
@@ -17,11 +17,7 @@ module load matplotlib/3.7.2-gfbf-2023a
 
 source tdmpc-env/bin/activate  # activate your virtual environment
 
-# Define the location model to be tested
-TASK_NAME=$(echo $1 | cut -d'-' -f1)
-TRAIN_ID=$2
-
-LOGDIR=checkpoints/train_${TRAIN_ID}
+LOGDIR=checkpoints
 
 # Run the code
-srun python src/test.py task=$1 checkpoint_dir=$LOGDIR/${TASK_NAME}
+srun python src/test.py task=$1 reward=$2 checkpoint_dir=$LOGDIR/$3
